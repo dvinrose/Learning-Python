9713d7c3066bb861750df93deedecb28482b4e6c
dvinrose <90020911+dvinrose@users.noreply.github.com>
2024-05-17 20:34:48 -0400

    Add files via upload

---
+++ Practice_Problem_4.1.py
@@ -0,0 +1,28 @@
+'''
+Start by executing the assignment:
+s = '0123456789'
+Now write expressions using string s and the indexing operator that evaluate to:
+(a) '234'
+(b) '78'
+(c) '1234567'
+(d) '0123'
+(e) '789'
+'''
+
+s = '0123456789'
+
+# A -----
+print(s[2:5])
+
+# B -----
+print(s[7:9])
+
+# C -----
+print(s[1:8])
+
+# D -----
+print(s[0:4])
+
+# E -----
+
+print(s[7:])
