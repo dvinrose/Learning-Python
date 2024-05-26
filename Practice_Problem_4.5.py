eba6dc39209126cdce59b5522c5ad8bbf49a665a
dvinrose <90020911+dvinrose@users.noreply.github.com>
2024-05-25 22:27:16 -0400

    Add files via upload

--- Practice_Problem_4.5.py
+++ Practice_Problem_4.5.py
@@ -15,3 +15,12 @@ state = 'AS'
 zipcode = '09876'
 '''
 
+first = 'John'
+last = 'Doe'
+street = 'Main Street'
+number = 123
+city = 'AnyCity'
+state = 'AS'
+zipcode = '09876'
+
+print('{} {} \n{} {} \n{}, {} {}'.format(first, last, number, street, city, state, zipcode))
